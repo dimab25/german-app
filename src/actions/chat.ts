d46c39e6e039814c0f81d033e2c53de633e266f0
TheCodingTom <dastolto.t@gmail.com>
2025-04-06 14:59:08 +0200

    rendering chatbot messages

--- src/actions/chat.ts
+++ src/actions/chat.ts
@@ -2,19 +2,14 @@
 
 import { GoogleGenAI } from "@google/genai";
 
-// const GEMINI_API_KEY = process.env.GEMINI_API_KEY || "";
-
-async function normalChat(userMessage: string): Promise<string | null> {
+async function normalChat(userMessage: string): Promise<string | undefined> {
   const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
-  async function main() {
-    const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
-      contents: "Explain how AI works in a few words",
-    });
-    console.log(response.text);
-  }
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: userMessage,
+  });
 
-  main();
+  return response.text;
 }
 export default normalChat;
