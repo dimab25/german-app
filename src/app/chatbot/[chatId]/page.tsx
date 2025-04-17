75352ea39ffa2cb32533ecd3f551a800208da8fe
TheCodingTom <dastolto.t@gmail.com>
2025-04-17 11:11:06 +0200

    fetching single chat using client component

--- src/app/chatbot/[chatId]/page.tsx
+++ src/app/chatbot/[chatId]/page.tsx
@@ -1,46 +1,41 @@
 "use client";
 
+import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import { ChatMessage, RectangleSelection, SelectionStates } from "../page";
-import { useSession } from "next-auth/react";
-import styles from "../page.module.css";
+
+type MessagesType = {
+  content: string;
+  role: string;
+};
 
 function SingleChatPage() {
-  const [messages, setMessages] = useState<ChatMessage[]>([]);
+  const params = useParams();
+  const chatId = params.chatId as string;
+  const [messages, setMessages] = useState<MessagesType[]>([]);
 
   const getSingleChat = async () => {
-    const requestOptions = {
-      method: "GET",
-    };
-
-    const response = await fetch(
-      "http://localhost:3000/api/chats/67ff9c5fa4255c4c0df52d2b",
-      requestOptions
-    );
-
-    const result = await response.json();
-    console.log(result);
-    setMessages(result.data[0].messages);
+    try {
+      const response = await fetch(`http://localhost:3000/api/chats/${chatId}`);
+      const result = await response.json();
+      setMessages(result.data[0].messages);
+    } catch (error) {
+      console.error("Error fetching single chat:", error);
+    }
   };
 
   useEffect(() => {
     getSingleChat();
-  }, []);
+  }, [chatId]);
 
   return (
     <div>
-      {messages &&
-        messages.map((msg, index) => (
-          <div
-            key={index}
-            className={`${styles.singleMessageContainer} ${
-              msg.role === "user" ? styles.userMessage : styles.otherMessage
-            }`}
-          >
-            <strong>{msg.role === "user" ? "You:" : "Bot:"}</strong>{" "}
-            <span>{msg.content}</span>
-          </div>
-        ))}
+      <h1>This is chat {chatId}</h1>
+      {messages.map((msg, index) => (
+        <div key={index} className={`message ${msg.role}`}>
+          <strong>{msg.role === "user" ? "You:" : "Bot:"}</strong>{" "}
+          <span>{msg.content}</span>
+        </div>
+      ))}
     </div>
   );
 }
